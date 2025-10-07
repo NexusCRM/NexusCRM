eeab6d48d91d805ae336a4decb3d8fb661d3e05b
NexusCRM <hermesspower@gmail.com>
2025-10-07 10:59:35 +0200

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,22 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
 
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Inter', sans-serif;
+  background-color: #f8fafc;
+}
+</style>
